8a52772edab08d735923258a39686149fc910cf0
JeongEuiwang <98511234+JeongEuiWang@users.noreply.github.com>
2025-06-04 16:39:24 +0900

    feat: add txHash column to swMileageHistory

--- src/models/swMileageTokenHistory.model.js
+++ src/models/swMileageTokenHistory.model.js
@@ -33,6 +33,10 @@ module.exports = (sequelize, DataTypes) => {
           type: DataTypes.STRING,
           allowNull: false,
       },
+      transaction_hash: {
+          type: DataTypes.STRING,
+          allowNull: false,
+      },
       note: {
           type: DataTypes.STRING,
           allowNull: true,
