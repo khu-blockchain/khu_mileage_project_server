ce85bc35c0c7540945109dfc0a0508b1e2c6c1fb
JeongEuiWang <euiwang0323@gmail.com>
2025-09-30 22:28:09 +0900

    feat: remote-server

--- src/main.ts
+++ src/main.ts
@@ -32,6 +32,7 @@ async function bootstrap() {
       'http://localhost:5174',
       'http://khunggum.khu.ac.kr',
       'https://khunggum.khu.ac.kr',
+      'http://khunggum.khu.ac.kr:8443',
       'http://163.180.140.219:8443'
     ],
     credentials: true,
